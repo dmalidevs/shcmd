0c691d5b2f1403f2f813a0edc4376a5b0416d2a8
dmalidevs <dmalidevs@gmail.com>
2024-02-11 06:58:54 +0600

    configured for laravel herd

--- makewp.sh
+++ makewp.sh
@@ -24,11 +24,9 @@ cd $INSTALL_DIR
 # Use directory name as the database name
 DB_NAME=$(echo $DIR_NAME | tr -cd '[:alnum:]' | tr '[:upper:]' '[:lower:]')
 
-# MySQL command to create the database
-mysql -u $DB_USER -p$DB_PASS -e "CREATE DATABASE $DB_NAME;"
 
 # Secure the site using Valet with HTTPS
-valet secure
+herd secure
 
 # Check if WordPress files are already present
 if [ -f "wp-config.php" ]; then
@@ -43,6 +41,9 @@ SITE_NAME="My WordPress Site"
 ADMIN_USER="admin"
 ADMIN_PASS="admin"
 
+# MySQL command to create the database
+mysql -u $DB_USER -p$DB_PASS -e "CREATE DATABASE $DB_NAME;"
+
 # Create wp-config.php using wp config create
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=localhost --extra-php <<PHP
 define( 'WP_DEBUG', false );
@@ -53,15 +54,15 @@ PHP
 wp core install --url=https://$DIR_NAME.test --title="$SITE_NAME" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASS" --admin_email=admin@example.com
 
 # Open the directory in Visual Studio Code
-if [[ "$1" == "-v" ]]; then
-    code $INSTALL_DIR
-fi
+# if [[ "$1" == "-v" ]]; then
+#     code $INSTALL_DIR
+# fi
 
 
 # Open the login page with pre-filled username and password
-if [[ "$1" == "-w" ]]; then
-	open "https://$DIR_NAME.test/wp-login.php?username=$ADMIN_USER&password=$ADMIN_PASS"
-fi
+# if [[ "$2" == "-w" ]]; then
+	open "https://$DIR_NAME.test/wp-login.php"
+# fi
 
 # Provide a message indicating the process is complete
 echo "WordPress installation and database creation complete. Your site is now secured with HTTPS in $INSTALL_DIR. Visual Studio Code is now open for editing. The login page is also opened in your default web browser with pre-filled username and password."
